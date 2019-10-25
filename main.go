47fe4109337e7a62287379f0167821935d25e2ef
Matt Long <matt@burythehammer.com>
2019-10-25 22:58:55 +0100

    Go modules and basic main scrape example

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+import "github.com/gocolly/colly"
+
+
+func main() {
+	Scrape()
+}
+
+func Scrape(){
+	c := colly.NewCollector()
+
+	// Find and visit all links
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		e.Request.Visit(e.Attr("href"))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Visit("http://go-colly.org/")
+}
